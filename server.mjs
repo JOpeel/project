a23ec70f84e6b86d63bff240528e54030fe11dcc
JOPeel <J.O.Peel2@newcastle.ac.uk>
2025-03-27 14:57:50 +0000

    commenting and cleaning up

--- server.mjs
+++ server.mjs
@@ -6,39 +6,39 @@ import fs from 'fs'
 import { Gpio } from 'onoff'
 import {createServer} from 'node:http'
 import {Server} from 'socket.io'
-// import pm2 from 'pm2'
 
-const app = express();
-const server = createServer(app);
-const io = new Server(server)
+const app = express(); //the server uses the express node.js framework
+const server = createServer(app); //creates the server
+const io = new Server(server) //initialises server for socket.io
 
+/*
+The GPIO interfaces for the buttons are stored in an array for easy reference
+*/
+let locationButton = [] 
 
-let locationButton = []
-
-locationButton[0] = new Gpio(520, 'in', 'both', {debounceTimeout : 50})
+locationButton[0] = new Gpio(520, 'in', 'both', {debounceTimeout : 50}) //debounceTimeout prevents 1 button press from registering multiple times
 locationButton[1] = new Gpio(519, 'in', 'both', {debounceTimeout : 50})
 locationButton[2] = new Gpio(521, 'in', 'both', {debounceTimeout : 50})
 
 
 
 var today = undefined
-setDate()
+setDate() //today's date and time must be initially set 
 
 
 
-var dateInterval = setInterval(setDate, 60000)
+var dateInterval = setInterval(setDate, 60000) //the date and time are rechecked every minute
 
 function setDate(){
   today = new Date()
-  //console.log("setdate")
-  //console.log(today.getHours())
-        var year    = today.getFullYear();
+  
+        var year    = today.getFullYear(); //values from year to second are fetched 
         var month   = today.getMonth(); 
         var day     = today.getDate();
         var hour    = today.getHours();
         var minute  = today.getMinutes();
         var second  = today.getSeconds(); 
-        if(month.toString().length == 1) {
+        if(month.toString().length == 1) { //values that aren't double digit are padded with 0 for correct formatting
              month = '0'+month;
         }
         if(day.toString().length == 1) {
@@ -53,17 +53,19 @@ function setDate(){
         if(second.toString().length == 1) {
              second = '0'+second;
         }   
-        //console.log(hour)
-        today = new Date(year, month, day, hour, minute, second) 
-        //console.log(today)
+        
+        today = new Date(year, month, day, hour, minute, second) //the date is reassembled using these values
+        
 }   
 
 
-let fileString = "["
+let fileString = "[" //the start of the json written to events.json
 
-let dateparts  = []
-// let testdate = new Date(parts[2], parts[1] - 1, parts[0])
-// console.log(testdate)
+let dateparts  = [] //the date is temporarily stored in an array of it's parts i.e. year, month, day
+
+/*
+The GPIO interfaces for the LEDs are stored in an array for easy reference, and 0 (off) is written to each
+*/
 
 let locationled = []
 
@@ -71,112 +73,81 @@ locationled[0] = new Gpio(514, 'out')
 locationled[1] = new Gpio(515, 'out')
 locationled[2] = new Gpio(516, 'out')
 
-locationled[0].writeSync(0)
-locationled[1].writeSync(0)
-locationled[2].writeSync(0)
+locationled.forEach((led) => led.writeSync(0))
 
-const currentFolder = path.dirname(fileURLToPath(import.meta.url))
+const currentFolder = path.dirname(fileURLToPath(import.meta.url)) //the currentfolder is declared to reference other files
 
-const noOfEvents = 0
-var events = [];
-var newevents = []
-var location1events = []
-var location2events = []
+var events = []; //events are stored as an array
+var newevents = [] //newevents is used to compare with events where if events have changed then the page is refreshed. 
+var location1events = [] //events are also stored in an array corresponding to their location, to be sent via socketio to events.ejs so that html elements can be referred to through their event name
+var location2events = [] 
 var location3events = []
 
-let locations = [false, false, false]
+let locations = [false, false, false] //whether a location has an upcoming event or not is stored as a boolean
 
-var eventdates = [];
-var i = 0;
+var eventdates = []; //the dates of all events are stored in a separate array to check which events are within next 48 hours for changing LEDs
+var i = 0; //referred to when iterating through events
 
-var eventsfromfile = fs.readFileSync('events.json', 'utf-8')
+var eventsfromfile = fs.readFileSync('events.json', 'utf-8') //the events.json file is read
 
-let eventsfromfilearray = eventsfromfile.split(',,,, ')
+let eventsfromfilearray = eventsfromfile.split(',,,, ') //'//// ' is the separator for items in the json
 
-eventsfromfilearray[0] = eventsfromfilearray[0].slice(1)
-eventsfromfilearray[eventsfromfilearray.length-1] = eventsfromfilearray[eventsfromfilearray.length-1].slice(0, eventsfromfilearray[eventsfromfilearray.length-1].length-1)
+eventsfromfilearray[0] = eventsfromfilearray[0].slice(1) //removes the '[' from the first element in the events read from the file
+eventsfromfilearray[eventsfromfilearray.length-1] = eventsfromfilearray[eventsfromfilearray.length-1].slice(0, eventsfromfilearray[eventsfromfilearray.length-1].length-1) //removes the "]" from the last element in the events read from the file
 
-//console.log(eventsfromfilearray[0])
+
 if (eventsfromfilearray[0] != ""){
-  eventsfromfilearray.forEach(pusheventsfromfile)
-
-  
+  eventsfromfilearray.forEach(pusheventsfromfile) //if the file isn't empty then the events from the file are pushed to events.
 }
 
-checkEvents()
+checkEvents() //an initial check of the event read from the file, including setting eventdates, location, location123events, and lED.
 
-var eventInterval = setInterval(checkEvents, 60000)
+var eventInterval = setInterval(checkEvents, 60000) //this is repeated every minute to dynamically update
 
-function pusheventsfromfile(file){
+function pusheventsfromfile(file){ 
     
-    events.push(JSON.parse(file))
-
-    // if (events.pop().location == "Location_1"){
-    //   location1events.push()
-    // }
+    events.push(JSON.parse(file)) //JSON.parse automatically parses the text content in a JSON format to a JSON object
     
 }
 
-function setlocationsActive(event){
- 
- 
-
-  //console.log(typeof(eventdates[i]))
-  //console.log(typeof(today))
-  
-  //console.log(eventdates[i])
-  //console.log(today)
-
-  //console.log(eventdates[i] - today)
+function setlocationsActive(event){ //sets the locations to true and LEDs to active if they are within the next 48 hours and not in the past
 
   if(((eventdates[i] - today) < 172800000) && ((eventdates[i] - today) > 0)){
+    
     locations[Number(event.location.split('_')[1]) - 1] = true
-    locationled[Number(event.location.split('_')[1]) - 1].writeSync(1)
-    //console.log("ledon")
+    locationled[Number(event.location.split('_')[1]) - 1].writeSync(1) //writes to LED
+   
   }
-  
-
-
   
   i += 1
 
 }
 
-
-
 function checkEvents(){
-  //console.log(events)
-  //console.log("checking events")
+
+  i = 0 //ensures iteration is restarted 
+
+  events.forEach(setDateTimes) //updates dates array
+
   i = 0
-  events.forEach(setDateTimes)
-
-  
-
-
-    //eventdates[i] = dateparts[0] + '-' + (dateparts[1]) + '-' + dateparts[2] + " " + events[events.length - 1].starttime + ":" + "00"
-
-    i = 0
     
-
-    //console.log(eventdates[i])
-    newevents = []
-    events.forEach(setlocationsActive)
+  newevents = [] //newevents is emptied
+  events.forEach(setlocationsActive) //sets location array and leds
     
-    if(events != newevents){
-      refreshPage()
-    }
+  if(events != newevents){ //refreshes the page if the events have changed
+    refreshPage()
+  }
     
-    i = 0
+  i = 0 //clears iterator
 
-    location1events = []
-    location2events = []
-    location3events = []
+  location1events = [] //clears events array for each location.
+  location2events = []
+  location3events = []
 
-    events.forEach(setLocationsArray)
-    //console.log(location1events)
+  events.forEach(setLocationsArray) //sets events array for each location
 }
 
-function setLocationsArray(event){
+function setLocationsArray(event){ //pushes the respective events to the array for each locaion
   if (event.location == "Location_1"){
     location1events.push(event)
   }
@@ -188,93 +159,82 @@ function setLocationsArray(event){
   }
 }
 
-function setDateTimes(){
-  
-    //console.log(i)
-    console.log(events[i])
-    dateparts = events[i].date.split("-")
-    var tempstarttime = events[i].starttime.split(":")
-    eventdates[i] = new Date(dateparts[0], dateparts[1] - 1, dateparts[2], tempstarttime[0], tempstarttime[1], "00")
-    //console.log(eventdates[i])
+function setDateTimes(){  //the dates array is set to contain he date of each event
 
-    // console.log(eventdates[i])
-    // console.log(today)
-    if (eventdates[i] < today){
-      // console.log(events[i])
-      // console.log("event removed")
+    dateparts = events[i].date.split("-") //the main part of the date is split by '-'
+    var tempstarttime = events[i].starttime.split(":") //a local array is used to store the minutes and seconds components of the start time
+    eventdates[i] = new Date(dateparts[0], dateparts[1] - 1, dateparts[2], tempstarttime[0], tempstarttime[1], "00") //a new date is added to the array which is made up of the year, month, day, minutes and seconds. (and 00 for ms)
+   
+    if (eventdates[i] < today){ //if the new date is before today, then the location is written as false, and the led turned off
+    
       locations[Number(events[i].location.split('_')[1]) - 1] = false
       locationled[Number(events[i].location.split('_')[1]) - 1].writeSync(0)
-      //console.log("ledoff")
+    
+      events.splice(i, 1) //the event is removed from the array
 
-      // delete events[i]
-      // console.log(events[i].date)
-      events.splice(i, 1)
     } else {
-      newevents.push(events[i])
-      //events = newevents
-      //refreshPage()
+
+      newevents.push(events[i]) //the event is pushed to newevents if it is still valid. If this equals events then nothing has changed and the page doesn't need to be refreshed.
+
     }
-
-      //console.log("popped")check
     
-    i+=1
-
-   
-    
+    i+=1 
+     
 }
 
+function formatDesc(string){ //this formats event descripions so that long descriptions have newlines instead of stretching out of the unscrolled browser
 
-app.use(express.urlencoded({ extended: true }))
-app.engine('ejs', ejs.renderFile)
-
-app.get('/form', (req,res) => {
-  res.render('form.ejs')
-})
-
-function formatDesc(string){
-
-  let noOfChars = 0
-  let newDesc = ""
-  for (let c = 0; c < string.length; c++) {
-    if ((noOfChars % 140) != 0 ){
-      newDesc += string.charAt(c)
+  let noOfChars = 0 //counts characters in the description
+  let newDesc = "" //an empty new description
+  for (let c = 0; c < string.length; c++) { //iterates through each character in the description
+    if ((noOfChars % 140) != 0 ){//checks if 140th char in string
+      newDesc += string.charAt(c) //prints 139 characters normally
     } else {
-      newDesc += string.charAt(c)
+      newDesc += string.charAt(c) //then prints the 140th charater with a /n
       newDesc += "\n"
     }
-  noOfChars +=1
+  noOfChars +=1 //adds to number of characters
   }
 
   return string
 }
 
-app.post('/form', (req, res) => {
-  events.push({
+
+app.use(express.urlencoded({ extended: true })) //this allows for posted forms to be accessed through req.body
+app.engine('ejs', ejs.renderFile) //tells express to use ejs.renderFile to render .ejs files lke the form and events
+
+app.get('/form', (req,res) => { //this sets up a route for a http get request to /form
+  res.render('form.ejs') //express renders form.ejs
+})
+
+app.post('/form', (req, res) => {//this sets up a route for a http post request to /form
+  events.push({ //the event booked is pushed to the events array
     name: req.body.event_name,
     location: req.body.location,
     date: req.body.date,
     starttime: req.body.time,
-    description: formatDesc(req.body.description)
+    description: formatDesc(req.body.description) //with a formatted desc
   })
 
-  checkEvents()
+  checkEvents() //ensures dynamic updating of events on form submission from another client
 
-  events.forEach(makeString)
-  fileString = fileString.slice(0,-5)
-  fileString += "]"
-  
-  
-  fs.writeFileSync("events.json", fileString)
-  
-  res.redirect('/booked')
+  events.forEach(makeString) //the new event, and others, are written to the json file
 
-  refreshPage()
+  fileString = fileString.slice(0,-5) //removes ',,,, ' from end of filestring
+  fileString += "]" //closes filestring with ']' to ensure array
+  
+  
+  fs.writeFileSync("events.json", fileString) //writes filestring to file
+  
+  res.redirect('/booked') //redirects to booked page
+
+  refreshPage() //refreshes page to ensure correct behaviour
   
 })
 
 function makeString(event){
-  fileString += JSON.stringify(event)
-  fileString += ",,,, "
+  fileString += JSON.stringify(event) //converts each event into a string and adds to filestring to be written to file
+  fileString += ",,,, " //adds separator
 }
 
 app.get('/events', (req,res) => {
@@ -292,47 +252,31 @@ function refreshPage(){
 }
 
 io.on('connection', (socket) => {
-  
-  //console.log ('a client connected');
-
-  //socket.emit('hello', 'world');
 
   locationButton[0].watch((err, value) => {
     if (value == 0){
-      console.log("serverbutton1pressed")
-      //console.log(location1events[0])
+    
       socket.emit('serverbutton1pressed', location1events)
       
-      
-      
     }
-
   })    
 
   locationButton[1].watch((err, value) => {
     if (value == 0){
-      console.log("serverbutton2pressed")
-      //console.log(location2events)
+    
       socket.emit('serverbutton2pressed', location2events)
       
     }
-    
   })    
 
   locationButton[2].watch((err, value) => {
     if (value == 0){
-      console.log("serverbutton3pressed")
+     
       socket.emit('serverbutton3pressed', location3events)
+      
     }
     
   })    
-
-  
-
-  socket.on('disconnect', () => {
-
-    //console.log("a client disconnected");
-  })
 
 });
 
